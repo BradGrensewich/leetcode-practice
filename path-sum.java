f27c952ad586d580c887b1fe1e223320d019f000
BradGrensewich <bradgrensewich@gmail.com>
2026-05-04 10:18:17 +0700

    exercise 112

---
+++ path-sum.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }    
+        int newTarget = targetSum - root.val;
+        if (newTarget == 0 && root.left == null && root.right == null) {
+            return true;
+        }
+        if (hasPathSum(root.left, newTarget)) {
+            return true;
+        }
+        return hasPathSum(root.right, newTarget);
+
+    }
+}
